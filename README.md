9f3a9719429c451449d5caf17517ceae2792e7b1
Ryan Dennehy <rmd5947@rit.edu>
2015-09-21 16:45:53 -0400

    Added substance to README, added msgs_by_likes and docstrings to the program

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # groupme-analysis
+
+A suite of functions to extract statistics about your GroupMe group chat
+
+## Running the program
+
+This program works with GroupMe data in JSON format, which is gathered through 
+the GroupMe API. I used a tool called [groupme-tools](https://github.com/cdzombak/groupme-tools) 
+by cdzombak on GitHub, which I highly recommend.
+
+## Requirements
+
+Works with Python 3.x, not 2.x
+
+However, in its current form, this program could probably be edited to work with 
+Python 2.x very easily
--- groupme-analysis.py
+++ groupme-analysis.py
@@ -1,64 +1,74 @@
 import json, re, collections, sys
-from collections import Counter, defaultdict
+from collections import defaultdict
 
 """
 File: groupme-analysis.py
 Author: Ryan Dennehy <rmd5947 @ rit.edu>
 """
 
-    #counts = collections.Counter([message['name'] for message in data])
-
-    #for mem in counts.most_common():
-    #    print("{0}: {1}/{2} = {3}".format(mem[0], str(l_c[mem[0]]), str(mem[1]), str(l_c[mem[0]]/mem[1])))
-
-    #for message in data:
-    #    if message['name'] == "Ryan Dennehy":
-    #        print("{0}: {1} likes".format(message['text'], len(message['favorited_by'])))
-
-    #likes = collections.Counter(itertools.chain.from_iterable([message['favorited_by'] for message in data]))
-    #likes = collections.Counter([len(message['favorited_by']) for message in data])
-    #print(likes)
-    #for message in data:
-    #    if len(message['favorited_by']) > 5:
-    #        print(len(message['favorited_by']))
-    #        print(message)
-    #d={}
-    #for message in data:
-    #    if message['name'] in d:
-    #        if message['picture_url'] is not None:
-    #            d[message['name']] += 1
-    #    else:
-    #        if message['picture_url'] is not None:
-    #            d[message['name']] = 1
-    #d_s = sorted(zip(d.values(), d.keys()), reverse=True)
-    #priint(d_s)
-
+# Changes the ordering of the output, 
+# e.g. from alphabetical order to likes
 by_2nd = lambda x: x[-1]
 
 def num_images(data):
+    """
+    Prints the total number of images sent to the group
+    Note: only counts attached images in its current state
+    """
     num_images = sum(msg['picture_url'] is not None for msg in data)
     print("{} images".format(num_images))
 
-def num_images_by_user(data, sorting="name", reverse=False):
+def num_images_by_user(data, sorting='name', reverse=False):
+    """
+    Prints the number of images each user has sent
+    
+    Keywork arguments:
+    sorting -- how the output should be sorted (default by name)
+    reverse -- reverse the ordering (default False)
+    """
     d = defaultdict(int)
     for msg in data:
         if msg['picture_url'] is not None:
             d[msg['name']] += 1
-    if sorting == "likes":
+    if sorting == 'likes':
         print("{}".format(str(sorted(d.items(), key=by_2nd, reverse=reverse))))
     else:
         print("{}".format(str(sorted(d.items(), reverse=reverse))))
 
-def likes_received(data, sorting="name", reverse=False, ignore_self=False):
+def msgs_by_likes(data):
+    """Prints the number of posts that have a number of likes (0, 1, ...)"""
+    d = defaultdict(int)
+    for msg in data:
+        d[len(msg['favorited_by'])] += 1
+    print(sorted(d.items()))
+
+def likes_received(data, sorting='name', reverse=False, ignore_self=False):
+    """
+    Prints the number of likes each user has received
+    
+    Keyword arguments:
+    sorting -- how the output should be sorted (default by name)
+    reverse -- reverse the ordering (default False)
+    ignore_self -- ignore self likes (default False)
+    """
     d = defaultdict(int)
     for msg in data:
         d[msg['name']] += len(msg['favorited_by'])
-    if sorting == "likes":
+    if sorting == 'likes':
         print("{}".format(str(sorted(d.items(), key=by_2nd, reverse=reverse))))
     else:
         print("{}".format(str(sorted(d.items(), reverse=reverse))))
 
-def likes_per_message(data, sorting="name", reverse=False, ignore_self=False):
+def likes_per_message(data, sorting='name', reverse=False, ignore_self=False):
+    """
+    Prints user, total likes received, total messages sent, 
+    and ratio of likes to messages
+    
+    Keyword arguments:
+    sorting -- how the output should be sorted (default by name)
+    reverse -- reverse the ordering (default False)
+    ignore_self -- ignore self likes (default False)
+    """
     likes = defaultdict(int)
     msgs  = defaultdict(int)
     for msg in data:
@@ -74,19 +84,34 @@ def likes_per_message(data, sorting="name", reverse=False, ignore_self=False):
 
 
 def set_of_ids(data):
+    """
+    Returns a set of all user ids
+    Will be useful for relating messages to ids rather than names, 
+    which users can change on a whim
+    """
     s = set()
     for message in data:
         if message['user_id'] not in s:
             s.add(message['user_id'])
+    return s
 
 def num_messages(data):
+    """Returns the total number of messages sent to the group"""
     return len(data)
 
 def main():
+    """
+    Main program
+    
+    Reads a filename from standard input, opens it, and does analysis 
+    on the contents
+    """
     filename = input("Filename: ")
+    #funcs = ['num_images', 'num_images_by_user', 'likes_received', 
+    #         'likes_per_message', 'set_of_ids', 'num_messages']
     with open(filename) as f:
         data = json.load(f)
-        likes_per_message(data)
+        #<insert function calls here>
 
 if __name__ == "__main__":
     main()
